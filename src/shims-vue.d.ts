c4fb2bf25f3b619e25f7fd58c0ca99e2efbf44f9
NiRongFei <fei332085762@aliyun.com>
2021-04-20 17:14:31 +0800

    test(deps): 添加ts文件处理范围

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,13 @@
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
+
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+// declare module '*.vue' {
+//   import { ComponentOptions } from 'vue'
+
+//   const componentOptions: ComponentOptions
+//   export default componentOptions
+// }
